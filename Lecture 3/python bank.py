e994907f02b93fab60044d0885210f54fd89d117
AvanFabian <avanfabiandaniswara@gmail.com>
2022-09-17 08:35:03 +0700

    latihan 3

---
+++ Lecture 3/python bank.py
@@ -0,0 +1,10 @@
+#Lecture3 Assignment
+
+greeting = input("Say hello : ").strip().lower()
+
+if greeting == "hello":
+    print("$0")
+elif greeting.startswith:
+    print("$20")
+else:
+    print("$100")
