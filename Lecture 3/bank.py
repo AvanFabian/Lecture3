30fa8d17942337af56c1cb502729336fea186a8b
AvanFabian <avanfabiandaniswara@gmail.com>
2022-09-17 09:40:46 +0700

    latihan 3

---
+++ Lecture 3/bank.py
@@ -0,0 +1,10 @@
+#Lecture3 Assignment
+
+answer = input("Greeting : ").strip().lower()
+
+if answer == "hello" or answer == "hello, Newman":
+    print("$0")
+elif answer.startswith:
+    print("$20")
+else:
+    print("$100")
