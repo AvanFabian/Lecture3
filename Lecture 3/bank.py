624d458f31ce106d1a46ff0ea7f913f8274307f8
AvanFabian <113287159+AvanFabian@users.noreply.github.com>
2022-09-17 14:47:00 +0700

    Update bank.py

--- Lecture 3/bank.py
+++ Lecture 3/bank.py
@@ -4,7 +4,7 @@ answer = input("Greeting : ").strip().lower()
 
 if answer == "hello" or answer == "hello, Newman":
     print("$0")
-elif answer.startswith:
+elif answer.startswith("h"):
     print("$20")
 else:
     print("$100")
